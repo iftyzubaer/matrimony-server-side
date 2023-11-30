69a6503d8ffb23c5e3c1dd4b1a5fc7ded7be2145
iftyzubaer <iftyzubaer1304.com>
2023-11-30 11:01:10 +0600

    send user data to database

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority;`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gsq0j5x.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +23,23 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const biodataCollection = client.db('biodataDB').collection('biodata')
 
+    app.post('/biodatas', async (req, res) => {
+      const newBiodata = req.body
+      const userEmail = newBiodata.email
+      const existingUser = await biodataCollection.findOne({ email: userEmail })
+      if (existingUser) {
+        const result = await biodataCollection.updateOne({ email: userEmail }, { $set: newBiodata })
+        res.send(result)
+      }
+      else {
+        const userCount = await biodataCollection.countDocuments()
+        const newUser = { user_id: userCount + 1, newBiodata }
+        const result = await biodataCollection.insertOne(newUser)
+        res.send(result)
+      }
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -38,9 +53,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Server Connected Successfully")
+  res.send("Server Connected Successfully")
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 })
